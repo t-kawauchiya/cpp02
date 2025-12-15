e54777600b809c50dc8fae085329fd9532bd7aab
takawauc <takawauc@student.42tokyo.jp>
2025-12-15 14:45:19 +0900

    (working)

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takawauc <takawauc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 12:40:30 by takawauc          #+#    #+#             */
+/*   Updated: 2025/12/15 14:18:19 by takawauc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _FIXED_H_
+#define _FIXED_H_
+
+#include <iostream>
+
+class Fixed
+{
+public:
+  Fixed(void);
+  Fixed(int integer);
+  Fixed(double float_num);
+  Fixed(const Fixed& other);
+  Fixed& operator=(const Fixed& other);
+  ~Fixed(void);
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+
+  float toFloat(void) const;
+  int toInt(void) const;
+
+private:
+  int _rawBits;
+  static const int _frac_bits = 8;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& c);
+
+#endif
