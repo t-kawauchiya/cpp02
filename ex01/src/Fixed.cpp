553be3cac39c9d57045e063e3b084adb76318dc4
takawauc <takawauc@student.42tokyo.jp>
2026-02-03 18:51:28 +0900

    refactor ex01

--- ex01/src/Fixed.cpp
+++ ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: takawauc <takawauc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 12:46:39 by takawauc          #+#    #+#             */
-/*   Updated: 2025/12/15 14:44:14 by takawauc         ###   ########.fr       */
+/*   Updated: 2026/02/03 18:49:30 by takawauc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Fixed::Fixed(int integer) : _rawBits(integer << 8)
   std::cout << "Int constructor called\n";
 }
 
-Fixed::Fixed(double float_num) : _rawBits(std::round(float_num * (1 << _frac_bits)))
+Fixed::Fixed(double float_num) : _rawBits(round(float_num * (1 << _frac_bits)))
 {
   std::cout << "Float constructor called\n";
 }
@@ -52,10 +52,6 @@ void Fixed::setRawBits(int const raw)
   std::cout << "setRawBits member function called\n";
   this->_rawBits = raw;
 }
-
-float Fixed::toFloat(void) const {}
-
-int Fixed::toInt(void) const {}
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
